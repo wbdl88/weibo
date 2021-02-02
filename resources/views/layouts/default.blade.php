d4ce86f9c0c648a4a57f5896ef5dc091e98fd0be
wbdl88 <491343024@qq.com>
2021-02-02 06:34:20 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','Weibo App')</title>
+  <title>@yield('title','Weibo App') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="Weibo App" class="navbar-brand"></a>
+      <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
+
 </body>
 </html>
