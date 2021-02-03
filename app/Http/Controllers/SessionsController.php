ce6dbd37a33e358fd11f138dd41bd79147d6ceb3
wbdl88 <491343024@qq.com>
2021-02-03 14:00:43 +0000

    访问限流

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -12,6 +12,11 @@ class SessionsController extends Controller
         $this->middleware('guest',[
             'only' => ['create']
         ]);
+
+        $this->middleware('throttle:10,10', [
+            'only' => ['store']
+        ]);
+
     }
 
     public function create()
