98fb34a8f11ab4ddc93517118ab25c4f24eb49e7
wbdl88 <491343024@qq.com>
2021-02-03 02:31:51 +0000

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -21,6 +21,12 @@
                         <label for="password">密码：</label>
                         <input type="password" class="form-control" name="password" value="{{ old('password') }}">
                     </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+                            <label for="exampleCheck1" class="form-check-label">记住我</label>
+                        </div>
+                    </div>
 
                     <button class="btn btn-primary" type="submit">登录</button>
                 </form>
