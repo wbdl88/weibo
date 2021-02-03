ebee4f6ac76e07dd9c34cbfe55f44986e5f5d485
wbdl88 <491343024@qq.com>
2021-02-03 05:52:22 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except'=> ['show', 'create', 'store']
+            'except'=> ['show', 'create', 'store','index']
         ]);
 
         $this->middleware('guest',[
@@ -19,6 +19,11 @@ class UsersController extends Controller
         ]);
     }
 
+    public function index()
+    {
+        $users = User::paginate(6);
+        return view('users.index', compact('users'));
+    }
     //显示注册页面
     public function create()
     {
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //填充50条数据
+        User::factory()->count(50)->create();
+        $user = User::find(1);
+        $user->name = 'tql';
+        $user->email = 'tql@qq.com';
+        $user->save();
+    }
+}
